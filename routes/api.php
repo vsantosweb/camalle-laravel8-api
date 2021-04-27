41e5dd20e2e384fc967cb170806d83284fa6ab63
Vitor Santos <vsantosweb@bitbucket.org>
2021-04-27 14:46:37 -0300

    adicionado documento do swagger

--- routes/api.php
+++ routes/api.php
@@ -20,4 +20,9 @@ Route::prefix('v1')->namespace('Api\v1')->group(function () {
 
         Route::get('message', 'DiscQuestionsController@index');
     });
+
+    Route::get('/swagger.json', function () { return response()->json(json_decode(file_get_contents(public_path('swagger-doc.json'))), 200)
+        ->header('access-control-allow-methods', 'GET, POST, DELETE, PUT')
+        ->header('access-control-allow-origin', '*')
+        ->header('Access-Control-Allow-Headers', 'Content-Type, api_key, Authorization'); });
 });
