d2cf013ccf69de4013b8fb340275229da1d61ba9
Vitor Santos <vsantosweb@bitbucket.org>
2021-06-09 11:32:03 -0300

    ajuste websockets config

--- config/websockets.php
+++ config/websockets.php
@@ -127,7 +127,9 @@ return [
         /*
          * Passphrase for your local_cert file.
          */
-        'passphrase' => env('LARAVEL_WEBSOCKETS_SSL_PASSPHRASE', null),
+        'passphrase' => env('JWT_SECRET', null),
+
+        'verify_peer' => false,
     ],
 
     /*
